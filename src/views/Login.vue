a0d47e031ecf010c47e432c757116ea8d3b3448a
MrXu <645352780@qq.com>
2019-02-23 10:23:35 +0800

    修改登录界面输入内容，样式错乱问题

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class='login'>
-    <img src="../assects/images/icon/logo.png" alt="一龄图标">
-    <div class="input-panel">
-      <input type="text" placeholder="请输入手机号">
+  <div class='dg-login'>
+    <div class='login'>
+      <img src="../assects/images/icon/logo.png" alt="一龄图标">
+      <div class="input-panel">
+        <input type="text" placeholder="请输入手机号">
+      </div>
+      <div class="input-panel">
+        <input type="text" placeholder="请输入验证码">
+        <mt-button type="default" size="small">获取验证码</mt-button>
+      </div>
+      <mt-button type="primary" class="mint-button--yellow"  size="large" @click="goback()">登陆</mt-button>
+      <div class="agreement">登陆即代表您已同意<span class="text">《用户注册协议》</span>和<span class="text">《隐私政策》</span></div>
     </div>
-    <div class="input-panel">
-      <input type="text" placeholder="请输入验证码">
-      <mt-button type="default" size="small">获取验证码</mt-button>
-    </div>
-    <mt-button type="primary" class="mint-button--yellow"  size="large" @click="goback()">登陆</mt-button>
-    <div class="agreement">登陆即代表您已同意<span class="text">《用户注册协议》</span>和<span class="text">《隐私政策》</span></div>
   </div>
 </template>
 <script>
@@ -28,40 +30,48 @@ export default {
 };
 </script> 
 <style lang="stylus">
-.login {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+.dg-login {
   background: white;
-  height 100vh
+  height: 100vh;
+  width: 100vw;
 
-  img {
-    width: 2rem;
-    margin-bottom: 0.6rem;
-  }
-
-  .input-panel {
-    box-sizing: border-box;
-    margin-top: 0.4rem;
-    width: 90%;
-    border-bottom: 1px solid rgb(188, 188, 188);
-    padding: 0.2rem;
+  .login {
+    padding-top: .6rem;
     display: flex;
-    justify-content: space-between;
-  }
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: white;
+    
+    img {
+      width: 2rem;
+      margin-bottom: 0.6rem;
+    }
 
-  .mint-button--yellow {
-    margin-top: 0.8rem;
-    width: 80%;
-  }
+    .input-panel {
+      box-sizing: border-box;
+      margin-top: 0.4rem;
+      width: 90%;
+      border-bottom: 1px solid rgb(188, 188, 188);
+      padding: 0.2rem;
+      display: flex;
+      justify-content: space-between;
+      height: 1rem;
+    }
 
-  .agreement {
-    margin-top: 3.6rem;
+    .mint-button--yellow {
+      margin-top: 0.8rem;
+      width: 80%;
+    }
 
-    .text {
-      color: #fee009;
+    .agreement {
+      margin-top: 2.6rem;
+
+      .text {
+        color: #fee009;
+      }
     }
   }
 }
+
 </style>
